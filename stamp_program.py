8b9f94ae347a8d438384db6d4bc9a78887021e85
mtello5 <mtello5@mail.fresnostate.edu>
2021-04-12 17:11:55 -0700

    added more code/edit

--- stamp_program.py
+++ stamp_program.py
@@ -6,15 +6,18 @@ round answer to next number
 output the result to the user
 end
 """
+import math
+#input: sheet
 def calculate(sheet):
     answer = sheet / 5
     rounded = round(answer)
     print("sheet is : ", answer)
     print("The answer is: ", rounded)
+    print("rounded is: ", rounded)
     print("================================")
+    # output: number of stamps for sheets
     return rounded
 
 output = calculate(10000)
-
-print("the return statement is: ", output)
+print("the number of stamps needed is: ", output)
 
